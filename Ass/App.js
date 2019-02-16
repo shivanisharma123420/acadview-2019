145f20ac7e09b06447dc3af9f35989aeb998cbdc
shivanisharma123420 <32288304+shivanisharma123420@users.noreply.github.com>
2019-02-16 16:11:31 +0530

    Update App.js

--- Ass/App.js
+++ Ass/App.js
@@ -8,8 +8,8 @@ class App extends Component {
       <div className = "App">
         <h1>My react app!</h1>
         <h3>This is my first react app</h3>
-        <Display name ="Drishti" age = "19" month = "March" />
-        <Display name ="Naman" age = "17" month = "December" />
+        <Display name ="Shivani" age = "20" month = "Oct" />
+        <Display name ="Aditi" age = "20" month = "Oct" />
 
       </div>
     );
