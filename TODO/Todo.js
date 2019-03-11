de13800bfa4b5a8f35d07149bfe1f5f96b7a8fd0
shivanisharma123420 <32288304+shivanisharma123420@users.noreply.github.com>
2019-03-11 22:04:38 +0530

    Add files via upload

---
+++ TODO/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Todo = ({ todos }) => {
+  const todoList = todos.length ? todos.map (todo => {
+    return(
+       <div className='collection-item' key= {todo.id}>
+       <span>{todo.item}</span>
+       </div>
+         )
+  }) : (
+      <p> Yay! I dont have anything to do! </p>
+    )
+    return (
+      <div className='todos collection'>
+      {todoList}
+      </div>
+    )
+}
